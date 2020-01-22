9736437e1feb9639e4b79171c85c349c12a8b665
seyoung <airseyoung@naver.com>
2020-01-23 02:25:38 +0900

    Add : Signin page, SigninForm component
    로그인 페이지, 컴포넌트 추가 및 css 구현

---
+++ conference-assistant/src/components/SigninForm.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="sign-in-form">
+       <form @submit.prevent="submit">
+           <fieldset>
+               <input type="text" placeholder="아이디를 입력하세요."/>
+               <input type="password" placeholder="비밀번호를 입력하세요."/>
+               <div></div>
+               <p><input type="checkbox"/><span> 로그인 상태 유지</span></p>
+               <div></div>
+               <button type="submit">Login</button>
+               <div><p>비밀번호가 생각나지 않으세요?</p></div>
+               <button>Join</button>
+
+           </fieldset>
+       </form>
+    </div>
+</template>
+<script>
+export default {
+    name: 'SigninForm'
+}
+</script>
+<style scoped>
+
+
+</style>f
