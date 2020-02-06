13b54f2d3b88218895032181fa0496efd1c8e74e
seyoung <airseyoung@naver.com>
2020-02-06 20:37:23 +0900

    Revert "Merge remote-tracking branch 'origin/feature/frontend' into feature/frontend"
    This reverts commit ab99d494e326ae0527ae4d3ceb1ec01eb48afe24.

---
+++ conference-assistant/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import AppHeader from './components/AppHeader'
+export default {
+  name: 'App',
+  components:{
+    AppHeader
+  }
+}
+</script>
+
+<style>
+/* @import 'http://localhost:8080/assets/index.css'; */
+@import 'assets/index.css';
+</style>
