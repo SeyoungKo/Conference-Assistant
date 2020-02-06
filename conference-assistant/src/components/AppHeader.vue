6bf6924a2dfcd9d786a0a2e6f5dbe6c717ed7d36
seyoung <airseyoung@naver.com>
2020-02-06 20:40:11 +0900

    Revert "Fix : AppHeader component"
    This reverts commit 7303b655b87620d19160c3a15730de3d62e8dd57.

---
+++ conference-assistant/src/components/AppHeader.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="app-header">
+        <h3>test</h3>
+
+    </div>
+</template>
+<script>
+export default {
+    name : 'AppHeader'
+}
+</script>
+<style scoped>
+.app-header{
+  position: fixed;
+   top: 0;
+   left: 0;
+   width: 100%;
+   height: 60px;
+   border-bottom: 1px solid #ededed;
+   background-color: rgb(250, 248, 248);
+}
+</style>
---
+++ conference-assistant/src/router/index.js
@@ -0,0 +1,34 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Signin from '../pages/Signin'
+import Signup from '../pages/Signup'
+import TeamListPage from '../pages/TeamListPage'
+//헤더 컴포넌트
+import AppHeader from '../components/AppHeader'
+
+Vue.use(Router)
+
+export default new Router({
+  mode : 'history',
+  routes: [
+    {
+      path:'/teamlist',
+      name:'TeamListPage',
+      components:{
+        header : AppHeader,
+        default: TeamListPage
+      }
+    },
+    {
+      path: '/',
+      name: 'Signin',
+      component: Signin
+    },
+    {
+      path:'/signup',
+      name: 'Signup',
+      component: Signup
+    },
+
+  ]
+})
