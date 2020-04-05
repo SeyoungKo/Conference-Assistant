3e5c53a5be7235d1905212c84e135130179885d0
seyoung <airseyoung@naver.com>
2020-04-06 05:55:51 +0900

    Fix : package.json dependancy
    bootstrap vue 삭제 package.json 변경

--- frontend/conference-assistant-frontend/src/components/FileStorageForm.vue
+++ frontend/conference-assistant-frontend/src/components/FileStorageForm.vue
@@ -1,23 +1,25 @@
 <template>
     <div class="file-storage-form">
-        <!-- <b-select name="file-sort">
+        <select name="file-sort">
             <option value="">타입선택</option>
             <option value="등록일자">등록일자</option>
             <option value="위치">위치</option>
-        </b-select>
-        <b-button name="download-btn" style="width:8%; background-color:rgb(223, 223, 223); border: 1px solid rgb(223, 223, 223); color:black;">다운로드</b-button>
-        <b-button name="delete-btn" style="margin-top:-1px; margin-left:3px; width:8%; background-color:rgb(223, 223, 223); border: 1px solid rgb(223, 223, 223); color:black;">삭제</b-button>
-        <b-table>
-            <b-tr>
-                <b-td>test</b-td>
-            </b-tr>
-             <tr>
-                <td>test</td>
+        </select>
+        <button name="download-btn" style="margin-left:80%;width:8%; background-color:rgb(223, 223, 223); border: 1px solid rgb(223, 223, 223); color:black; padding-top:5px; padding-bottom: 5px; border-radius:0.2rem; font-size:14px;">다운로드</button>
+        <button name="delete-btn" style="margin-top:-1px; margin-left:3px; width:8%; background-color:rgb(223, 223, 223); border: 1px solid rgb(223, 223, 223); color:black; padding-top:5px; padding-bottom: 5px;border-radius:0.2rem; font-size:14px;">삭제</button>
+        <table class="table">
+            <tr class="table-header-tr">
+                <td>작성자</td>
+                <td>파일명</td>
+                <td>위치</td>
+                <td>등록일자</td>
             </tr>
              <tr>
-                <td>test</td>
+                 <td></td>
+                <td style="text-align:center; padding-left:15px; font-weight:600;">    데이터가 없습니다.</td>
             </tr>
-        </b-table> -->
+
+        </table>
     </div>
 </template>
 <script>
@@ -26,7 +28,7 @@ export default {
 }
 </script>
 <style scope>
-/* .file-storage-form{
+.file-storage-form{
    z-index:0;
    position: fixed;
    margin-top:1.8%;
@@ -38,12 +40,36 @@ export default {
    border: 1px solid #eeeeee;
    border-radius: 1.2rem;
 }
-.custom-select{
+select{
     margin-left: 20px;
     margin-top:15px;
-    width:140px;
+    width: 200px;
+    padding: .8em .5em;
+    border: 1px solid rgb(223, 223, 223);
+    border-radius:1rem;
+    font-family: inherit;
+    background: url('../img/select_drop.png') no-repeat 93% 30%;
+    border-radius: 0px;
+    -webkit-appearance: none;
+    -moz-appearance: none;
+    appearance: none;
+
 }
-.btn.btn-secondary {
+#download-btn{
     margin-left: 80%;
-} */
+}
+.table{
+    margin-top:30px;
+    margin-left: 50px;
+    width:1000px;
+    height:400px;
+    background-color:#f5f5f5;
+}
+.table-header-tr{
+    background-color:#32a852;
+    color:white;
+    font-weight: 600;
+    text-align: center;
+    height:35px;
+}
 </style>
