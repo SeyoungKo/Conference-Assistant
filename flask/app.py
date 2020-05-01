28a82db1866f52665a6eac85a2a5c488cb90abd7
seyoung <airseyoung@naver.com>
2020-05-01 22:57:58 +0900

    Add : flask socket.io
    flask Socket.io 설정

--- flask/app.py
+++ flask/app.py
@@ -1,19 +1,36 @@
-from flask import Flask
+import os
 import sys
-sys.path.insert(0,"db.py")
-import db
+import os
+from flask import Flask, render_template, session
+from flask_socketio import SocketIO, emit
 
-# sys.path.append("../database/")
-# from database import database
+import model
+# sys.path.insert(0,"model.py")
 
 app = Flask(__name__)
-
+socketio = SocketIO(app, cors_allowed_origins="*")
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
 
+model
+
+@socketio.on('connect', namespace='/corekeyword') # namespace : 연결할 socket 식별
+def connect():
+    if(model.rtn_keyword!=''):
+        emit("response", {'message':'Connected', 'keyword':model.rtn_keyword, 'room_idx': '1'}) # socket 연결시 room_idx를 확인한다.
+
+@socketio.on('disconnect', namespace='/corekeyword')
+def disconnect():
+    session.clear()
+    print("Socket Disconnected")
+
+@socketio.on("request", namespace='/coreKeyword')
+def request(message):
+    emit("response", {'message': message['data'],'room_idx':'1'}, broadcast=True)
 
 if __name__ == '__main__':
+    model.main()
     app.run()
-    db.main()
+
---
+++ flask/model.py
@@ -0,0 +1,166 @@
+import os
+import re
+import sys
+import threading
+import csv
+import app
+
+from numpy import matrix
+from pymongo import MongoClient
+
+# from textmining_dataset.lovit_textmining_dataset.navernews_10days import get_news_paths as lv
+from soynlp.utils import DoublespaceLineCorpus
+
+from soynlp.noun import LRNounExtractor
+# from soynlp.tokenizer import NounLMatchTokenizer
+# from soynlp.tokenizer import NounMatchTokenizer
+
+from soynlp.tokenizer import RegexTokenizer
+from collections import Counter
+from konlpy.tag import Okt
+
+client = MongoClient()
+# 클래스 객체 할당
+
+matrix = []
+
+f = open('/Users/seyoung/Conference-Assistant/Conference-Assistant/flask/chat_corpus.csv','r')
+csvReader = csv.reader(f)
+# 단어 경계학습을 위한 챗봇(대화체) 데이터 코퍼스
+
+for row in csvReader:
+    matrix.append(row)
+    # print(row)
+f.close()
+
+client = MongoClient('mongodb://localhost:27017/local')
+
+count =0
+tmp_data = ''
+text=''
+corpus = None
+noun_scores = None
+cnt =0
+rtn_keyword = ''
+
+# match_tokenizer = NounMatchTokenizer(noun_scores=noun_scores)
+
+# 쓰레드로 가져온 최근 값
+
+# DB_HOST = 'XXX.XX.XX.XXX:27017'
+# DB_ID = 'root'
+# DB_PW = 'PW'
+# client = MongoClient('mongodb://%s:%s@%s' % (DB_ID, DB_PW, DB_HOST))
+
+class AsyncTask:
+
+    def __init__(self):
+        pass
+
+    def TaskA(self):
+
+        global count
+        global tmp_data
+        global text
+        global corpus
+        global noun_scores
+        global cnt
+        global rtn_keyword
+        # 전역변수 사용
+
+        db = client["local"]
+        # db 객체 할당받기
+
+        chats = db["chats"]
+        # db에서 collection 이름으로 객체를 생성
+
+        # post={
+        #     "id" : "mike",
+        #     "age" : 20,
+        #     "text" : "테스트 도큐먼트입니다1."
+        # }
+
+        coll = db.chats
+        # users 변수에 해당하는 collection 이름으로 할당된다.
+
+        # coll.insert(post)
+
+        coll_list = db.list_collection_names()
+        # collection 목록 보기
+
+        # for chats in coll.find():
+        #     rtn_chats =chats.get('contents')
+        #
+        #
+        #     print(text)
+
+        cursor = db.chats.find().sort([('created_at', -1)]).limit(1)
+        docs = list(cursor)
+
+        for doc in docs:
+            if doc !=" " and count==0:
+                rtn_msg = doc["contents"]
+
+                tmp_data = rtn_msg
+                # print(tmp_data)
+                count += 1
+
+            elif doc !=" " and tmp_data != doc["contents"] :
+                rtn_msg = doc["contents"]
+                tmp_data = rtn_msg
+
+                # print(tmp_data)
+                text = re.sub('[-=+,#/\?:^$.@*\"※~&%ㆍ!』\\‘|\(\)\[\]\<\>`\'…》]', '', tmp_data)
+                # print("re:" + text)
+
+                tokenizer = RegexTokenizer()
+                # print(tokenizer.tokenize(text))
+
+                okt = Okt()
+                noun = okt.nouns(text)
+                cnt = Counter(noun)
+                # 명사 빈도 추출
+
+                noun_list = cnt.most_common(3)
+                for v in noun_list:
+                    rtn_keyword = v
+                    print(v)
+
+                count += 1
+
+                # path = lv(date='2016-10-20')
+                # corpus = DoublespaceLineCorpus(matrix, iter_sent=True)
+                # noun_extractor = LRNounExtractor()
+                # nouns= noun_extractor.train_extract(text)
+                #
+                # noun_scores = {noun:score.score for noun, score in noun_scores.items()}
+                #
+                # lmatch_tokenizer= NounLMatchTokenizer(nouns=noun_scores)
+                # match_tokenizer = NounMatchTokenizer(noun_scores=noun_scores)
+                #
+                # print(lmatch_tokenizer.tokenize(matrix))
+                # for word in text.split():
+                #     print(word, word in noun_scores)
+
+        threading.Timer(3,self.TaskA).start()
+
+def main():
+    at = AsyncTask()
+    at.TaskA()
+    count=0
+
+if __name__ =='__main_':
+    main()
+# res = json.dumps(chats, default=str)
+# json 직렬화 (serialize)
+
+# chats collection에서 불러온 json타입 데이터에서 contents 문자열 가져오기
+
+# def cleanText(rtn_chats):a
+#     for msg in (str)(rtn_chats.values()):
+#         text = re.sub('[-=+,#/\?:^$.@*\"※~&%ㆍ!』\\‘|\(\)\[\]\<\>`\'…》]', '',msg)
+#         print(text)
+#         return text
+
+# cleanText(chats)
+
